422747d78f3829a986724fe11feb1193ba652c45
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-08-31 14:38:22 +0200

    Connection to db

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/user.routes";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const server = new Server();
 
 server.app.use("/user", userRoutes);
+
+mongoose.connect(
+  process.env.MONGO_DB_CONNECTION_STRING as string,
+  {},
+  (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Database online");
+  }
+);
 
 server.start(() => console.log(`Server running on port ${server.port}`));
