755f25e53d87274ee943d2a7823555695a1b7652
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-09-07 13:33:15 +0200

    Get paginated posts

--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -16,4 +16,19 @@ postRoutes.post("/", [verifyToken], async (req: Request, res: Response) => {
   res.json({ ok: true, post });
 });
 
+postRoutes.get("/", async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
+  let skip = page - 1;
+  skip *= 10;
+
+  const posts = await Post.find()
+    .sort({ _id: -1 })
+    .skip(skip)
+    .limit(10)
+    .populate("user", "-password")
+    .exec();
+
+  res.json({ ok: true, page, posts });
+});
+
 export default postRoutes;
